d6b67391b84c3efaa707ae205a499038440ffaf7
khushi-sarvaiya <khushisarvaiya410@gmail.com>
2024-09-19 00:15:20 +0530

    your about components added successfully

--- src/Comonents/About.jsx
+++ src/Comonents/About.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+};
 
-export default About
+export default About;
